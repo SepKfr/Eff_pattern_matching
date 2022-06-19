8211ac82e2a783f85d5800268fd9c6d453308ad9
anonymous-1992 <33998911+anonymous-1992@users.noreply.github.com>
2022-06-19 18:28:24 -0400

    Add files via upload
    Add data files

--- data/traffic.py
+++ data/traffic.py
@@ -15,7 +15,6 @@
 
 # Lint as: python3
 
-import pandas as pd
 import sklearn.preprocessing
 from Utils import utils, base
 
@@ -24,37 +23,19 @@ DataTypes = base.DataTypes
 InputTypes = base.InputTypes
 
 
-class ElectricityFormatter(GenericDataFormatter):
-    """Defines and formats data_set for the electricity dataset.
-    Note that per-entity z-score normalization is used here, and is implemented
-    across functions.
-    Attributes:
-    column_definition: Defines input and data_set type of column used in the
-      experiment.
-    identifiers: Entity identifiers used in experiments.
-    """
+class TrafficFormatter(GenericDataFormatter):
 
     _column_definition = [
-      ('id', DataTypes.REAL_VALUED, InputTypes.ID),
-      ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.TIME),
-      ('power_usage', DataTypes.REAL_VALUED, InputTypes.TARGET),
-      ('hour', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
-      ('day_of_week', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
-      ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
-      ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
+        ('id', DataTypes.REAL_VALUED, InputTypes.ID),
+        ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.TIME),
+        ('values', DataTypes.REAL_VALUED, InputTypes.TARGET),
+        ('time_on_day', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('day_of_week', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('hours_from_start', DataTypes.REAL_VALUED, InputTypes.KNOWN_INPUT),
+        ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
     ]
 
-    def __init__(self):
-        """Initialises formatter."""
-
-        self.identifiers = None
-        self._real_scalers = None
-        self._cat_scalers = None
-        self._target_scaler = None
-        self._num_classes_per_cat_input = None
-        self._time_steps = self.get_fixed_params()['total_time_steps']
-
-    def split_data(self, df, valid_boundary=1315, test_boundary=1339):
+    def split_data(self, df, valid_boundary=151, test_boundary=166):
         """Splits data_set frame into training-validation-test data_set frames.
         This also calibrates scaling object, and transforms data_set for each split.
         Args:
@@ -67,7 +48,7 @@ class ElectricityFormatter(GenericDataFormatter):
 
         print('Formatting train-valid-test splits.')
 
-        index = df['days_from_start']
+        index = df['sensor_day']
         train = df.loc[index < valid_boundary]
         valid = df.loc[(index >= valid_boundary - 7) & (index < test_boundary)]
         test = df.loc[index >= test_boundary - 7]
@@ -77,7 +58,6 @@ class ElectricityFormatter(GenericDataFormatter):
         return (self.transform_inputs(data) for data in [train, valid, test])
 
     def set_scalers(self, df):
-
         """Calibrates scalers using the data_set supplied.
         Args:
           df: Data to use to calibrate scalers.
@@ -90,28 +70,18 @@ class ElectricityFormatter(GenericDataFormatter):
         target_column = utils.get_single_col_by_input_type(InputTypes.TARGET,
                                                            column_definitions)
 
+        # Extract identifiers in case required
+        self.identifiers = list(df[id_column].unique())
+
         # Format real scalers
         real_inputs = utils.extract_cols_from_data_type(
             DataTypes.REAL_VALUED, column_definitions,
             {InputTypes.ID, InputTypes.TIME})
 
-        # Initialise scaler caches
-        self._real_scalers = {}
-        self._target_scaler = {}
-        identifiers = []
-        for identifier, sliced in df.groupby(id_column):
-
-          if len(sliced) >= self._time_steps:
-
-            data = sliced[real_inputs].values
-            targets = sliced[[target_column]].values
-            self._real_scalers[identifier] \
-          = sklearn.preprocessing.StandardScaler().fit(data)
-
-            self._target_scaler[identifier] \
-          = sklearn.preprocessing.StandardScaler().fit(targets)
-
-          identifiers.append(identifier)
+        data = df[real_inputs].values
+        self._real_scalers = sklearn.preprocessing.StandardScaler().fit(data)
+        self._target_scaler = sklearn.preprocessing.StandardScaler().fit(
+            df[[target_column]].values)  # used for predictions
 
         # Format categorical scalers
         categorical_inputs = utils.extract_cols_from_data_type(
@@ -122,17 +92,14 @@ class ElectricityFormatter(GenericDataFormatter):
         num_classes = []
         for col in categorical_inputs:
             # Set all to str so that we don't have mixed integer/string columns
-          srs = df[col].apply(str)
-          categorical_scalers[col] = sklearn.preprocessing.LabelEncoder()
-          categorical_scalers[col].fit(srs.values)
-          num_classes.append(srs.nunique())
+            srs = df[col].apply(str)
+            categorical_scalers[col] = sklearn.preprocessing.LabelEncoder().fit(
+                srs.values)
+            num_classes.append(srs.nunique())
 
         # Set categorical scaler outputs
         self._cat_scalers = categorical_scalers
         self._num_classes_per_cat_input = num_classes
-
-        # Extract identifiers in case required
-        self.identifiers = identifiers
 
     def transform_inputs(self, df):
         """Performs feature transformations.
@@ -142,14 +109,13 @@ class ElectricityFormatter(GenericDataFormatter):
         Returns:
           Transformed data_set frame.
         """
+        output = df.copy()
 
         if self._real_scalers is None and self._cat_scalers is None:
             raise ValueError('Scalers have not been set!')
 
-        # Extract relevant columns
         column_definitions = self.get_column_definition()
-        id_col = utils.get_single_col_by_input_type(InputTypes.ID,
-                                                    column_definitions)
+
         real_inputs = utils.extract_cols_from_data_type(
             DataTypes.REAL_VALUED, column_definitions,
             {InputTypes.ID, InputTypes.TIME})
@@ -157,17 +123,8 @@ class ElectricityFormatter(GenericDataFormatter):
             DataTypes.CATEGORICAL, column_definitions,
             {InputTypes.ID, InputTypes.TIME})
 
-        # Transform real inputs per entity
-        df_list = []
-        for identifier, sliced in df.groupby(id_col):
-            # Filter out any trajectories that are too short
-            if len(sliced) >= self._time_steps:
-                sliced_copy = sliced.copy()
-                sliced_copy[real_inputs] = self._real_scalers[identifier].transform(
-                    sliced_copy[real_inputs].values)
-                df_list.append(sliced_copy)
-
-        output = pd.concat(df_list, axis=0)
+        # Format real inputs
+        output[real_inputs] = self._real_scalers.transform(df[real_inputs].values)
 
         # Format categorical inputs
         for col in categorical_inputs:
@@ -184,24 +141,29 @@ class ElectricityFormatter(GenericDataFormatter):
           Data frame of unnormalised predictions.
         """
 
-        if self._target_scaler is None:
-            raise ValueError('Scalers have not been set!')
+        output = predictions.copy()
 
         column_names = predictions.columns
 
-        df_list = []
-        for identifier, sliced in predictions.groupby('identifier'):
-            sliced_copy = sliced.copy()
-            target_scaler = self._target_scaler[identifier]
-
-            for col in column_names:
-                if col not in {'identifier'}:
-                    sliced_copy[col] = target_scaler.inverse_transform(sliced_copy[col])
-            df_list.append(sliced_copy)
-
-        output = pd.concat(df_list, axis=0)
+        for col in column_names:
+            if col not in {'identifier'}:
+                output[col] = self._target_scaler.inverse_transform(predictions[col])
 
         return output
+
+    # Default params
+    def get_fixed_params(self):
+        """Returns fixed model parameters for experiments."""
+
+        fixed_params = {
+            'total_time_steps': 9 * 24,
+            'num_encoder_steps': 7 * 24,
+            'num_epochs': 50,
+            'early_stopping_patience': 5,
+            'multiprocessing_workers': 5
+        }
+
+        return fixed_params
 
     def get_default_model_params(self):
         """Returns default optimised model parameters."""
@@ -216,17 +178,6 @@ class ElectricityFormatter(GenericDataFormatter):
 
         return model_params
 
-    def get_fixed_params(self):
-        """Returns fixed model parameters for experiments."""
-
-        fixed_params = {
-            'total_time_steps': 9 * 24,
-            'num_encoder_steps': 7 * 24,
-            'num_epochs': 50,
-        }
-
-        return fixed_params
-
     def get_num_samples_for_calibration(self):
         """Gets the default number of training and validation samples.
         Use to sub-sample the data_set for network calibration and a value of -1 uses
@@ -235,3 +186,5 @@ class ElectricityFormatter(GenericDataFormatter):
           Tuple of (training samples, validation samples)
         """
         return 128000, 15360
+
+
