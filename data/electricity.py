794aaed7280dfc3600a7d75b0cdf79855bf93af6
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-06-19 18:47:22 -0400

    adding eff acat

--- data/electricity.py
+++ data/electricity.py
@@ -234,4 +234,4 @@ class ElectricityFormatter(GenericDataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 256, 256
+        return 128000, 15360
