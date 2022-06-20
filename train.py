6f09abd82a038efefcf72dbf864645214a84785f
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-06-20 12:28:21 -0400

    eff acat by leaning filer lengths

--- train.py
+++ train.py
@@ -165,8 +165,8 @@ def evaluate(config, args, test_en, test_de, test_y, test_id, criterion, formatt
 def main():
 
     parser = argparse.ArgumentParser(description="preprocess argument parser")
-    parser.add_argument("--attn_type", type=str, default='ACAT')
-    parser.add_argument("--name", type=str, default='ACAT')
+    parser.add_argument("--attn_type", type=str, default='basic_attn')
+    parser.add_argument("--name", type=str, default='basic_attn')
     parser.add_argument("--exp_name", type=str, default='electricity')
     parser.add_argument("--cuda", type=str, default="cuda:0")
     parser.add_argument("--seed", type=int, default=21)
