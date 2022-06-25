5d3a414f6ea93fb38e55687400c698a00ab9e05a
sepidehkoohfar <sepide1.koohfar@gamil.com>
2022-06-25 14:15:41 -0400

    trying

--- data/electricity.py
+++ data/electricity.py
@@ -224,7 +224,7 @@ class ElectricityFormatter(GenericDataFormatter):
         fixed_params = {
             'total_time_steps': 9 * 24,
             'num_encoder_steps': 10 * 24,
-            'num_epochs': 1,
+            'num_epochs': 50,
         }
 
         return fixed_params
