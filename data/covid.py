1883f0701e67bf0b53f0ef9ae56d9593d090fff0
anonymous-1992 <sepide1.koohfar@gmail.com>
2022-08-17 18:50:49 -0700

    remove early stopping and increasing the batch size

--- data/covid.py
+++ data/covid.py
@@ -1,7 +1,8 @@
 import sklearn.preprocessing
 from Utils import utils, base
+from data.electricity import ElectricityFormatter
 
-GenericDataFormatter = base.GenericDataFormatter
+GenericDataFormatter = ElectricityFormatter
 DataTypes = base.DataTypes
 InputTypes = base.InputTypes
 
@@ -19,122 +20,6 @@ class CovidFormatter(GenericDataFormatter):
         ('categorical_id', DataTypes.CATEGORICAL, InputTypes.STATIC_INPUT),
 
     ]
-
-    def __init__(self, pred_len):
-        """Initialises formatter."""
-
-        self.pred_len = pred_len
-
-    def transform_data(self, df):
-        """Splits data_set frame into training-validation-test data_set frames.
-        This also calibrates scaling object, and transforms data_set for each split.
-        Args:
-          df: Source data_set frame to split.
-          valid_boundary: Starting year for validation data_set
-          test_boundary: Starting year for test data_set
-        Returns:
-          Tuple of transformed (train, valid, test) data_set.
-        """
-
-        print('Formatting train-valid-test splits.')
-
-        self.set_scalers(df)
-
-        return self.transform_inputs(df)
-
-    def set_scalers(self, df):
-        """Calibrates scalers using the data_set supplied.
-        Args:
-          df: Data to use to calibrate scalers.
-        """
-        print('Setting scalers with training data_set...')
-
-        column_definitions = self.get_column_definition()
-        id_column = utils.get_single_col_by_input_type(InputTypes.ID,
-                                                       column_definitions)
-        target_column = utils.get_single_col_by_input_type(InputTypes.TARGET,
-                                                           column_definitions)
-
-        # Extract identifiers in case required
-        self.identifiers = list(df[id_column].unique())
-
-        # Format real scalers
-        real_inputs = utils.extract_cols_from_data_type(
-            DataTypes.REAL_VALUED, column_definitions,
-            {InputTypes.ID, InputTypes.TIME})
-
-        data = df[real_inputs].values
-        self._real_scalers = sklearn.preprocessing.StandardScaler().fit(data)
-        self._target_scaler = sklearn.preprocessing.StandardScaler().fit(
-            df[[target_column]].values)  # used for predictions
-
-        # Format categorical scalers
-        categorical_inputs = utils.extract_cols_from_data_type(
-            DataTypes.CATEGORICAL, column_definitions,
-            {InputTypes.ID, InputTypes.TIME})
-
-        categorical_scalers = {}
-        num_classes = []
-        for col in categorical_inputs:
-            # Set all to str so that we don't have mixed integer/string columns
-            srs = df[col].apply(str)
-            categorical_scalers[col] = sklearn.preprocessing.LabelEncoder().fit(
-                srs.values)
-            num_classes.append(srs.nunique())
-
-        # Set categorical scaler outputs
-        self._cat_scalers = categorical_scalers
-        self._num_classes_per_cat_input = num_classes
-
-    def transform_inputs(self, df):
-        """Performs feature transformations.
-        This includes both feature engineering, preprocessing and normalisation.
-        Args:
-          df: Data frame to transform.
-        Returns:
-          Transformed data_set frame.
-        """
-        output = df.copy()
-
-        if self._real_scalers is None and self._cat_scalers is None:
-            raise ValueError('Scalers have not been set!')
-
-        column_definitions = self.get_column_definition()
-
-        real_inputs = utils.extract_cols_from_data_type(
-            DataTypes.REAL_VALUED, column_definitions,
-            {InputTypes.ID, InputTypes.TIME})
-        categorical_inputs = utils.extract_cols_from_data_type(
-            DataTypes.CATEGORICAL, column_definitions,
-            {InputTypes.ID, InputTypes.TIME})
-
-        # Format real inputs
-        output[real_inputs] = self._real_scalers.transform(df[real_inputs].values)
-
-        # Format categorical inputs
-        for col in categorical_inputs:
-            string_df = df[col].apply(str)
-            output[col] = self._cat_scalers[col].transform(string_df)
-
-        return output
-
-    def format_predictions(self, predictions):
-        """Reverts any normalisation to give predictions in original scale.
-        Args:
-          predictions: Dataframe of model predictions.
-        Returns:
-          Data frame of unnormalised predictions.
-        """
-
-        output = predictions.copy()
-
-        column_names = predictions.columns
-
-        for col in column_names:
-            if col not in {'identifier'}:
-                output[col] = self._target_scaler.inverse_transform(predictions[col])
-
-        return output
 
     # Default params
     def get_fixed_params(self):
@@ -171,4 +56,4 @@ class CovidFormatter(GenericDataFormatter):
         Returns:
           Tuple of (training samples, validation samples)
         """
-        return 64000, 6400
+        return 128000, 12800
