bc6216cbb5f2a59ecfc84afd089a6bd7613118bc
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-09-10 11:53:23 -0400

    update covid with trip data

--- models/rnn.py
+++ models/rnn.py
@@ -18,20 +18,21 @@ class RNN(nn.Module):
         self.linear_enc = nn.Linear(src_input_size, hidden_size, bias=False)
         self.linear_dec = nn.Linear(tgt_input_size, hidden_size, bias=False)
         self.linear2 = nn.Linear(hidden_size, 1, bias=False)
+        self.hidden = None
         self.device = device
         np.random.seed(seed)
         random.seed(seed)
         torch.manual_seed(seed)
 
-    def forward(self, x_en, x_de, hidden=None):
+    def forward(self, x_en, x_de):
 
         x_en = self.linear_enc(x_en).permute(1, 0, 2)
         x_de = self.linear_dec(x_de).permute(1, 0, 2)
 
-        if hidden is None:
-            hidden = torch.zeros(self.n_layers, x_en.shape[1], self.hidden_size).to(self.device)
+        if self.hidden is None:
+            self.hidden = torch.zeros(self.n_layers, x_en.shape[1], self.hidden_size).to(self.device)
 
-        _, (hidden, state) = self.enc_lstm(x_en, (hidden, hidden))
+        _, (hidden, state) = self.enc_lstm(x_en, (self.hidden, self.hidden))
         dec_output, _ = self.dec_lstm(x_de, (hidden, hidden))
 
         outputs = self.linear2(dec_output).transpose(0, 1)
