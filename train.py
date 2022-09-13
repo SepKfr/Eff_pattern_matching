a78c87965a758eb254c730e1fdd8c7748ebddc4e
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-09-13 19:08:07 -0400

    fix the data leak problem

--- train.py
+++ train.py
@@ -212,7 +212,7 @@ class Train:
                             seed=self.seed, kernel=kernel)
         model.to(self.device)
 
-        optimizer = Adam(model.parameters(), lr=1e-4)
+        optimizer = NoamOpt(Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9), 2, d_model, 500)
 
         epoch_start = 0
 
@@ -230,7 +230,7 @@ class Train:
 
                 optimizer.zero_grad()
                 loss.backward()
-                optimizer.step()
+                optimizer.step_and_update_lr()
 
             print("Train epoch: {}, loss: {:.4f}".format(epoch, total_loss))
 
