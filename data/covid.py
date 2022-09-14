15c165665868708f87619b2c47bfb9a10067874a
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-09-14 12:31:37 -0400

    only using decoder and sort covid based on days from start

--- data/covid.py
+++ data/covid.py
@@ -61,7 +61,7 @@ class CovidFormatter(GenericDataFormatter):
 
         model_params = {
             'hidden_layer_size': [32, 64],
-            'minibatch_size': [128],
+            'minibatch_size': [256],
             'num_heads': 8,
             'stack_size': [1],
             'context_lengths': [1, 3, 6, 9]
