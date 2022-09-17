6d4ceb1a93fa1a5f862dac1883fbe78483583797
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-09-17 10:28:15 -0400

    removing learning rate warmup

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ class Train:
         if not os.path.exists(self.model_path):
             os.makedirs(self.model_path)
 
-        d_model = trial.suggest_categorical("d_model", [16, 65])
+        d_model = trial.suggest_categorical("d_model", [16, 64])
         stack_size = 1
 
         n_heads = self.model_params['num_heads']
