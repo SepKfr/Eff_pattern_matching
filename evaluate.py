dd7fd6a780ad31a1ef8d15c490376ca276a7357c
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-09-29 18:53:03 -0400

    adding evaluation

--- evaluate.py
+++ evaluate.py
@@ -94,13 +94,12 @@ predictions = torch.from_numpy(np.mean(predictions, axis=0))
 targets_all = torch.from_numpy(np.mean(targets_all, axis=0))
 
 results = torch.zeros(2, args.pred_len)
+normaliser = targets_all.abs().mean()
 
 for j in range(args.pred_len):
 
-    normaliser = targets_all[:, :, j].abs().mean()
     results[0, j] = mse(predictions[:, :, j], targets_all[:, :, j]) / normaliser
     results[1, j] = mae(predictions[:, :, j], targets_all[:, :, j]) / normaliser
 
-
 df = pd.DataFrame(results.detach().cpu().numpy())
-df.to_csv("{}_{}.csv".format(args.name, args.pred_len))
+df.to_csv("{}_{}_{}.csv".format(args.exp_name, args.name, args.pred_len))
