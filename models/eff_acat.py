f7c606b46de66c2f0399a0c5dc19d9c032d05a4a
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-10-08 14:48:22 -0400

    adding the process model ish

--- models/eff_acat.py
+++ models/eff_acat.py
@@ -256,10 +256,10 @@ class Decoder(nn.Module):
 
 
 class process_model(nn.Module):
-    def __init__(self, d, device, dr=0.1):
+    def __init__(self, d, device):
         super(process_model, self).__init__()
-
         self.mut = nn.Linear(d, 2*d, device=device)
+        self.proj_out = nn.Linear(d, d, device=device)
         self.softPlus = nn.Softplus()
         self.d = d
         self.device = device
@@ -267,11 +267,14 @@ class process_model(nn.Module):
     def forward(self, x):
 
         musig = self.mut(x)
-        mu, sigma = musig[:, :, :self.d], self.softPlus(musig[:, :, -self.d:])
-        var = self.softPlus(sigma)
-        g = torch.distributions.normal.Normal(mu, var)
-        pred = g.sample()
-        return pred, g
+        mu, sigma = musig[:, :, :self.d], musig[:, :, -self.d:]
+        sigma = self.softPlus(sigma)
+        g = mu + sigma * torch.normal(torch.zeros(mu.shape, device=self.device),
+                                      torch.ones(sigma.shape, device=self.device))
+        pred = self.proj_out(g)
+        mu = torch.median(pred, dim=0)[0]
+        sigma = pred.std(dim=0)
+        return pred, mu, sigma
 
 
 class Transformer(nn.Module):
@@ -312,14 +315,24 @@ class Transformer(nn.Module):
             if isinstance(m, nn.Linear):
                 nn.init.uniform_(m.weight, -1/np.sqrt(d_model), 1/np.sqrt(d_model))
 
+    def nll(self, loc, scale, value):
+
+        var = (scale ** 2)
+        log_scale = torch.log(scale)
+        loss = -((value - loc) ** 2) / (2 * var) - log_scale
+        loss = torch.mean(loss, dim=[0, 1, 2]) - math.log(math.sqrt(2 * math.pi))
+        kl = 0.5 * (loc ** 2 + scale ** 2 - log_scale - 1)
+        kl = torch.mean(kl, dim=[0, 1])
+        loss = loss - kl
+        return -loss
+
     def forward(self, enc_inputs, dec_inputs):
 
         if self.p_model:
 
             enc_inputs = self.enc_embedding(enc_inputs)
-            enc_outputs, dist = self.process(enc_inputs)
-            likelihood = dist.log_prob(enc_inputs)
-            gloss = -torch.mean(likelihood)
+            enc_outputs, mu, sigma = self.process(enc_inputs)
+            gloss = self.nll(mu, sigma, enc_inputs)
 
         else:
             enc_outputs = self.enc_embedding(enc_inputs)
