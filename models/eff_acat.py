fb067b9689a8f742237ddca927842ec54b18bfac
sepidekoohfar <sepideh.koohfar@unh.edu>
2022-10-17 14:53:23 -0400

    trying with conv

--- models/eff_acat.py
+++ models/eff_acat.py
@@ -277,7 +277,6 @@ class process_model(nn.Module):
         mu, sigma = musig[:, :, :self.d], musig[:, :, -self.d:]
         z = mu + torch.exp(sigma*0.5) * torch.randn_like(sigma, device=self.device)
         y = torch.tanh(self.decoder(z.permute(0, 2, 1)).permute(0, 2, 1))
-        print(y)
         return y
 
 
